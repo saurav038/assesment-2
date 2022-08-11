80f6ded7d00bec49645274731134f4baded0a9d5
saurav <saurav965355340@gmail.com>
2022-08-11 17:25:58 +0530

    implement pagination

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 const xhr = new XMLHttpRequest();
 const source =
   "https://jsonplaceholder.typicode.com/photos";
+  let data, page = 1;
 xhr.open("GET", source, true);
 xhr.onreadystatechange = () => {
   if (xhr.readyState === 4 && xhr.status === 200) {
-    const response = JSON.parse(xhr.responseText);
-    console.log(response);
+    data = JSON.parse(xhr.responseText);
     let output = "";
     for (let i = 0; i < 100; i++) {
       output += `
         <div class="grid-item">
-            <img src="${response[i].url}" class="ajaxImg">
+            <img src="${data[i].url}" class="ajaxImg">
             <div class="content">
                 <span><hr></span>
-                <p class="title">${response[i].title}</p>
+                <p class="title">${data[i].title}</p>
             </div>
         </div>`;
     }
     document.querySelector(".grid-container").innerHTML = output;
   }
 };
-xhr.send()
+xhr.send();
 
-const toggglePremium = () => {
-
-}
+// implementing pagination
+let pagination = new tui.Pagination("pagination", {
+    totalItems: 1000,
+  });
+  
+  pagination.on("beforeMove", function (eventData) {
+    page = eventData.page;
+    updateContent();
+  });
+  
+  function updateContent() {
+    let output = "";
+    for (let i = (page - 1) * 100; i < page * 100; i++) {
+      output += `
+      <div class="grid-item">
+      <img src="${data[i].url}" class="ajaxImg">
+      <div class="content">
+          <span><hr></span>
+          <p class="title">${data[i].title}</p>
+      </div>
+  </div>`;
+    }
+    document.querySelector(".grid-container").innerHTML = output;
+  }
